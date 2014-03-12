61f33cf220fe1aab93f6c499358dc23a92fce258
enter08 <demicredzep1990@gmail.com>
2014-03-12 10:56:11 +0100

    Minor changes

--- spec/integration/view_homepage_spec.rb
+++ spec/integration/view_homepage_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 feature 'View the homepage' do 
 	scenario 'user sees relevant information' do
 		visit root_path
-		expect(page).to have_css 'title', text: 'Expense manager'
+		expect(page).to have_css("title", text: "Expense manager")
 		expect(page).to have_css '[data-role="description"]'
 	end
 end
--- spec/support/sign_in_helpers.rb
+++ spec/support/sign_in_helpers.rb
@@ -10,7 +10,7 @@ module SignInHelpers
 		visit new_user_registration_path
 		fill_in 'Email', with: "person@example.com"
 		fill_in 'Password', with: "password"
-		fill_in 'Password confirmation', "password"
+		fill_in 'Password confirmation', with: "password"
 		click_button 'Sign up'
 	end
 
