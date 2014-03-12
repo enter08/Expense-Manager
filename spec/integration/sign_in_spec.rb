72627fc9eacbf1b3be2fb1e1939458639f47de1a
enter08 <demicredzep1990@gmail.com>
2014-03-12 10:25:03 +0100

    with helpers and sign in/up/out specs

--- spec/integration/sign_in_spec.rb
+++ spec/integration/sign_in_spec.rb
@@ -2,12 +2,8 @@ require 'spec_helper'
 
 feature 'Sign in as a user' do
 	scenario 'with an email address' do
-		FactoryGirl.create(:user)
-		visit new_user_session_path
-		fill_in 'Email', with: 'person@example.com'
-		fill_in 'Password', with: 'password'
-		click_button 'Sign in'
+		@user = FactoryGirl.create(:user)
+		sign_in(@user)
 		expect(page).to have_css "p", text: "Signed in"
 	end	
 end
-
