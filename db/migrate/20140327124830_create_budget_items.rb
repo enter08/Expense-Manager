3092a46fce9cda6173c565782dc4e71021c0c2f1
enter08 <demicredzep1990@gmail.com>
2014-04-03 11:18:39 +0200

    Minor edit

--- db/migrate/20140327124830_create_budget_items.rb
+++ db/migrate/20140327124830_create_budget_items.rb
@@ -1,5 +1,8 @@
 class CreateBudgetItems < ActiveRecord::Migration
   def change
-    create_table :budget_items
+    create_table :budget_items do |t|
+      t.decimal :value
+      t.date :budget_date
+    end
   end
 end
