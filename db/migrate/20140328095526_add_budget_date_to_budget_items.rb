d7c4d0a39faa95b7bef8479a00648f456d1dff5d
enter08 <demicredzep1990@gmail.com>
2014-04-03 11:23:38 +0200

    Minor edit4

--- db/migrate/20140328095526_add_budget_date_to_budget_items.rb
+++
@@ -1,5 +0,0 @@
-class AddBudgetDateToBudgetItems < ActiveRecord::Migration
-  def change
-    add_column :budget_items, :budget_date, :date
-  end
-end
--- db/migrate/20140328095544_add_value_to_budget_items.rb
+++
@@ -1,5 +0,0 @@
-class AddValueToBudgetItems < ActiveRecord::Migration
-  def change
-    add_column :budget_items, :value, :decimal
-  end
-end
