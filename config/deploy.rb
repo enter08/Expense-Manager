eeada37dc3110e39bad366d4de6ef0084901c1a2
enter08 <demicredzep1990@gmail.com>
2014-04-04 08:48:26 +0200

    With assets task

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,7 +46,7 @@ set :ssh_options, { :forward_agent => true }
 set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets}
 
 set(:config_files, %w(
   nginx_unicorn
